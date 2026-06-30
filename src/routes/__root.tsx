251ed56343b4c8ed680776287fe0a8c282430aae
SnowyRawrGamer <joel@freshideawebsites.com>
2026-06-29 21:54:40 -0400

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
+import { UsernameGate } from "@/components/shared/UsernameGate";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
